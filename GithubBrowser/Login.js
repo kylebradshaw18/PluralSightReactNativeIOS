a9ad48c38e9b9571cc4f96cf9e1e2ce5811d1e3f
Kyle Bradshaw <kylebradshaw18@gmail.com>
2017-06-07 00:07:55 -0400

    Left off on authservice

--- GithubBrowser/Login.js
+++ GithubBrowser/Login.js
@@ -4,31 +4,126 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  TextInput,
+  TouchableHighlight,
+  ActivityIndicator
 } from 'react-native';
+import AuthService from './AuthService';
 
-export default class Login extends React.Component {
+export default class Login extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			showProgress:false
+		};
+	}
   render() {
+	  var errorCtrl = <View/>;
+	
+	  if(!this.state.sucess && this.state.badCredentials){
+		  errorCtrl = <Text style={styles.error}>
+		  That username and password didn't work
+		  </Text>;
+	  } else if(!this.state.sucess && this.state.unknownError){
+		  errorCtrl = <Text style={styles.error}>
+		  We experienced an unexpected issue
+		  </Text>;
+	  }
+
     return (
       <View style={styles.container}>
 	  	<Image style={styles.logo}
-		  source={{ uri: 'https://www.brightedge.com/blog/wp-content/uploads/2016/02/HTTPs.jpg', width: 32, height: 32, }}
+		  	source={{uri: 'Octocat'}}
+		/>
+		<Text 
+			style={styles.heading}>
+				Github Browser
+		</Text>
+		<TextInput 
+			onChangeText={(text) => this.setState({username: text})}
+			style={styles.input}
+			placeholder="Github Username"
+		/>
+		<TextInput 
+			onChangeText={(text) => this.setState({password: text})}
+			style={styles.input}
+			placeholder="Github Password"
+			sourceTextEntry="true"
+		/>
+		<TouchableHighlight
+			onPress={this.onLoginPressed.bind(this)}
+			style={styles.button}>
+			<Text 
+				style={styles.buttonText}>
+					Log in
+			</Text>
+		</TouchableHighlight>
+		{errorCtrl}
+		<ActivityIndicator
+			animating={this.state.showProgress}
+			size="large"
+			style={styles.loader}
 		/>
 	  </View>
     );
+  }
+
+  onLoginPressed(){
+	  this.setState({showProgress: true});
+
+	  AuthService.login({
+		  username: this.state.username,
+		  password: this.state.password
+	  }, (results) => {
+		  this.setState(results);
+	  });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-	paddingTop: 40
+	paddingTop: 40,
+	alignItems: 'center',
+	padding: 15
   }, 
   logo:{
 	width: 66,
 	height: 55
+  },
+  heading: {
+	  fontSize: 30,
+	  marginTop: 10
+  },
+  input:{
+	  height: 50,
+	  marginTop: 10,
+	  padding: 4,
+	  fontSize: 18,
+	  borderWidth: 1,
+	  borderColor: '#48bbec',
+	  borderRadius: 0,
+	  color: '#48BBEC'
+  },
+  button:{
+	  height: 50,
+	  backgroundColor: '#48BBEC',
+	  alignSelf: 'stretch',
+	  marginTop: 10,
+	  justifyContent: 'center'
+  },
+  buttonText:{
+	  fontSize:22,
+	  color: '#FFF',
+	  alignSelf: 'center'
+  },
+  loader:{
+	  marginTop: 20
+  },
+  error:{
+	  color: 'red',
+	  paddingTop: 10
   }
 });
